6b7fb6eeeb5ae7f10741df8b8f2fd62c59d2debd
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2025-05-07 09:25:29 -0500

    Remove contract values from config constants

--- constants/Config.ts
+++ constants/Config.ts
@@ -3,5 +3,5 @@ export const RPC_ENDPOINT = "https://rpc.xion-testnet-2.burnt.com:443";
 export const REST_ENDPOINT = "https://api.xion-testnet-2.burnt.com";
 
 // Contract Addresses
-export const USER_MAP_CONTRACT_ADDRESS = "xion1xlsdw7en2a45v9z7stmfrml3f88zj6lrxcq5uk7vv4xy4wyhjdqqlgwqa8"; // Add your user map contract address here
-export const TREASURY_CONTRACT_ADDRESS = "xion1hcuf8dv4n2h6gffez3e0y70gjm20g72mtmcl3qf7t4q0laz5ef0shfm50z"; // Add your treasury contract address here 
+export const USER_MAP_CONTRACT_ADDRESS = ""; // Add your user map contract address here
+export const TREASURY_CONTRACT_ADDRESS = ""; // Add your treasury contract address here 

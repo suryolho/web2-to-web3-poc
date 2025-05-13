37520943e8508f7b8e4a6016a72006b3636948f5
Adrian Thompson <adrian.delroy.thompson@gmail.com>
2025-05-13 14:18:30 -0500

    Removal of the Config constant file

--- constants/Config.ts
+++
@@ -1,7 +0,0 @@
-// Network Configuration
-export const RPC_ENDPOINT = "https://rpc.xion-testnet-2.burnt.com:443";
-export const REST_ENDPOINT = "https://api.xion-testnet-2.burnt.com";
-
-// Contract Addresses
-export const USER_MAP_CONTRACT_ADDRESS = ""; // Add your user map contract address here
-export const TREASURY_CONTRACT_ADDRESS = ""; // Add your treasury contract address here 
